fd7ccc01c0fa85aa7332d8519a481ee265a8f096
ecstasy <31936449+zakrok@users.noreply.github.com>
2026-07-18 21:21:35 +0200

    ci: prime build for svelte 5 static build off-netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,18 @@
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-netlify';
-import preprocess from 'svelte-preprocess';
-
-
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
+	kit: { adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			precompress: false,
+			strict: true,
+			fallback: undefined
+		})
 	},
-	preprocess: preprocess()
+	preprocess: vitePreprocess(),
+	extensions: ['.svelte']
 };
 
 export default config;
